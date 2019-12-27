d944db024a7ec5b83c4c9ebf8e1e7ea2ce8e7025
Francois CONTE <francois.conte@odandb.com>
2019-12-27 11:50:47 +0100

    Add Bundle class

---
+++ OdandbSimpleCorsBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Odandb\SimpleCorsBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OdandbSimpleCorsBundle extends Bundle
+{
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $container->addCompilerPass(new DependencyInjection\Compiler\CorsConfigurationProviderPass());
+    }
+}
